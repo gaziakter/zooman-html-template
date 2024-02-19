883023da65262b9681ed7d363181afb2926a90d1
Gazi Akter <gaziakterdev@gmail.com>
2024-02-19 06:20:07 +0600

    upload new website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+ new WOW().init();
+
+
+//Menu slider
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
